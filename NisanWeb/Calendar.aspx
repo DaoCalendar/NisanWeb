892203a00653b61a64396cc6c3252a6c5955b69d
Yeangshing Then <yancyn@gmail.com>
2012-07-21 18:43:29 +0800

    added muslim calendar converter page.

---
+++ NisanWeb/Calendar.aspx
@@ -0,0 +1,52 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="Calendar.aspx.cs" Inherits="Calendar" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+<asp:UpdatePanel ID="UpdatePanel1" runat="server">
+        <ContentTemplate>
+        <div>
+            <asp:Calendar ID="Calendar1" runat="server" OnSelectionChanged="Calendar1_SelectionChanged" />
+            <asp:TextBox ID="TextBox1" runat="server" />
+            <asp:Button ID="Button1" runat="server" Text="&gt;&gt;" OnClick="Button1_Click" />
+            <asp:Label ID="Label1" runat="server" />
+        </div>
+        <div class="legend">
+            <h2>Muslim Month</h2>
+              <span class="float-left">
+                <ol>
+                    <li>Muharram</li>
+                    <li>Safar</li>
+                    <li>Rabiulawal</li>
+                    <li>Rabiulakhir</li>
+                    <li>Jamadilawal</li>
+                    <li>Jamadilakhir</li>
+                    <li>Rejab</li>
+                    <li>Syaaban</li>
+                    <li>Ramadhan</li>
+                    <li>Syawal</li>
+                    <li>Zulkaedah</li>
+                    <li>Zulhijjah</li>
+                </ol>
+            </span>
+            <span class="float-left">
+                <ol>
+                    <li>محرّم</li>
+                    <li>صفر</li>
+                    <li>ربيع الاول</li>
+                    <li>ربيع الاخير</li>
+                    <li>جمادالاول</li>
+                    <li>جمادالاخير</li>
+                    <li>رجب</li>
+                    <li>شعبان</li>
+                    <li>رمضان</li>
+                    <li>شوال</li>
+                    <li>ذوالقعده</li>
+                    <li>ذوالحجه</li>
+                </ol>
+            </span>
+        </div>
+        </ContentTemplate>
+</asp:UpdatePanel>
+</asp:Content>
+
---
+++ NisanWeb/Calendar.aspx.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using HLGranite.Nisan;
+
+public partial class Calendar : System.Web.UI.Page
+{
+    private MuslimCalendar calendar;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            DateTime date = DateTime.Now;
+            TextBox1.Text = date.Day + "/" + date.Month + "/" + date.Year;
+        }
+
+        string file = this.MapPath("App_Data\\muslimcal.xml");
+        calendar = new MuslimCalendar(ReadXml(file));
+    }
+    private DataTable ReadXml(string fileName)
+    {
+        DataTable table = new DataTable();
+        DataSet dataset = new DataSet();
+
+        try
+        {
+            if (System.IO.File.Exists(fileName))
+                dataset.ReadXml(fileName);
+            if (dataset.Tables.Count > 0)
+                table = dataset.Tables[0].Copy();
+
+            return table;
+        }
+        catch (Exception ex)
+        {
+            System.Diagnostics.Debug.WriteLine(ex);
+            return table;
+        }
+        finally { dataset.Dispose(); }
+    }
+    private DateTime CastToLocalDate(string sender)
+    {
+        DateTime output = DateTime.Now;
+        string[] pieces = sender.Split(new char[] { '/' });
+        if (pieces.Length != 3) return output;
+
+        output = new DateTime(Convert.ToInt32(pieces[2]), Convert.ToInt32(pieces[1]), Convert.ToInt32(pieces[0]));
+        return output;
+    }
+    private void ToMuslimDate(DateTime sender)
+    {
+        try
+        {
+            calendar.GetDate(sender);
+            Label1.Text = calendar.Day + "/" + calendar.Month + "/" + calendar.Year;
+        }
+        catch (Exception ex)
+        {
+            System.Diagnostics.Debug.WriteLine(ex);
+            return;
+        }
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        ToMuslimDate(CastToLocalDate(TextBox1.Text));
+    }
+    protected void Calendar1_SelectionChanged(object sender, EventArgs e)
+    {
+        TextBox1.Text = Calendar1.SelectedDate.Day + "/" + Calendar1.SelectedDate.Month + "/" + Calendar1.SelectedDate.Year;
+        ToMuslimDate(CastToLocalDate(TextBox1.Text));
+    }
+}
