36076b3e1eaa6be69203d54a5d08e60ffb9cdff9
yancyn <yancyn@gmail.com>
2012-08-06 23:48:10 +0800

    change get version from dll instead of manually.

--- NisanWeb/MasterPage.master.cs
+++ NisanWeb/MasterPage.master.cs
@@ -22,6 +22,60 @@ public partial class MasterPage : System.Web.UI.MasterPage
             Menu2.Visible = true;
             if (user is Admin) Menu3.Visible = true;
         }
+        if (!IsPostBack)
+            Initialize();
+    }
+    private void Initialize()
+    {
+        linVersion.Text = "ver " + GetAssemblyVersion("HLGranite.Nisan.DLL", 3);
+    }
+    /// <summary>
+    /// Get speciifed assembly version (ref AssemblyInfo.cs)
+    /// </summary>
+    /// <param name="assemblyName">Assembly name, eg.GISPM.DLL.</param>
+    /// <param name="digit">Digit to show (max 4 only).</param>
+    /// <returns></returns>
+    public string GetAssemblyVersion(string assemblyName, int digit)
+    {
+        string lVersion = "";
+        assemblyName = assemblyName.Trim().ToUpper();
+        if (digit > 4) digit = 4;//maximum
+
+        try
+        {
+            System.Reflection.Assembly[] lAssemblies = System.Threading.Thread.GetDomain().GetAssemblies();
+            foreach (System.Reflection.Assembly ass in lAssemblies)
+            {
+                if (ass.GetType() == typeof(System.Reflection.Assembly)
+                    && ass.CodeBase.ToUpper().LastIndexOf(assemblyName) > -1)
+                {
+                    string[] lSplits = ass.FullName.Split(' ');
+                    foreach (string s in lSplits)
+                    {
+                        if (s.Trim(',').ToLower().IndexOf("version") > -1)
+                        {
+                            string[] lVers = s.Trim(',').Split('=');
+                            lVersion = lVers[lVers.Length - 1];
+                            if (digit < 4) //only less than 4 digit
+                            {
+                                lVers = lVersion.Split('.');
+                                lVersion = "";//reset
+                                for (int i = 0; i < digit; i++)
+                                    lVersion += lVers[i] + ".";
+                            }
+                        }
+                    }
+                }//found specified dll only
+            }//end loops
+
+            return lVersion.TrimEnd('.');
+        }
+        catch (Exception ex)
+        {
+            System.Diagnostics.Debug.WriteLine(ex);
+            //logger.Error(ex);
+            return lVersion.TrimEnd('.');
+        }
     }
     protected void btnSearch_Click(object sender, EventArgs e)
     {
